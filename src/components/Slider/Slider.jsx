ce1ef8df123943ea90db2470fa682ce5c5fdf7b4
Valerii Pometun <vmpometun@gmail.com>
2023-10-25 13:42:10 +0300

    update slider styles

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -2,6 +2,8 @@ import { products } from '../../image/products';
 import 'react-image-gallery/styles/css/image-gallery.css';
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
+import css from './Slider.module.css';
+import { logo } from '../../image/logo';
 
 const images = [
   {
@@ -30,12 +32,33 @@ const images = [
 
 const Slider = ({ slides }) => {
   return (
-    <ImageGallery
-      items={images}
-      showFullscreenButton={false}
-      autoPlay={true}
-      showPlayButton={false}
-    />
+    <div className={css.container}>
+      <ImageGallery
+        items={images}
+        showFullscreenButton={false}
+        autoPlay={true}
+        showPlayButton={false}
+        slideInterval={4000}
+        renderLeftNav={(onClick, disabled) => (
+          <img
+            className={css.leftNav}
+            src={logo.leftArrow}
+            alt="slider"
+            onClick={onClick}
+            disabled={disabled}
+          />
+        )}
+        renderRightNav={(onClick, disabled) => (
+          <img
+            className={css.rightNav}
+            src={logo.rightArrow}
+            alt="slider"
+            onClick={onClick}
+            disabled={disabled}
+          />
+        )}
+      />
+    </div>
   );
 };
 
--- src/image/logo.js
+++ src/image/logo.js
@@ -1,4 +1,5 @@
 import mainLogo from './logo/logo.jpg';
 import welder from './logo/welder.png';
-
-export const logo = { mainLogo, welder };
+import leftArrow from './logo/left.png';
+import rightArrow from './logo/right.png';
+export const logo = { mainLogo, welder, leftArrow, rightArrow };
